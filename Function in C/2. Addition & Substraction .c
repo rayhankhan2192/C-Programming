f0b84dd0473aa3d00b18c6de5a9d74b21ee3dce2
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-27 18:00:33 +0600

    Addition & Substractions

--- Function in C/2. Addition & Substraction .c
+++ Function in C/2. Addition & Substraction .c
@@ -1,10 +1,19 @@
 #include<stdio.h>
-void sum(int a,int b,int c){
-    printf("The Addition is = %d\n",a+b+c);}
-void sub(int a,int b,int c){
+void sum(int a,int b,int c)
+{
+    printf("The Addition is = %d\n",a+b+c);
+}
+void sub(int a,int b,int c)
+{
     printf("The Substraction is = %d\n",a-b-c);
-int main(){
-    printf("\n\t\t===== Addition =====\n\n");
-   sum(5,5,5); sum(10,5,5); sum(20,10,5);
-    printf("\n\t\t===== Substraction =====\n\n");
-   sub(20,10,5); sub(25,5,5); sub(30,15,8);}
+    int main()
+    {
+        printf("\n\t\t===== Addition =====\n\n");
+        sum(5,5,5);
+        sum(10,5,5);
+        sum(20,10,5);
+        printf("\n\t\t===== Substraction =====\n\n");
+        sub(20,10,5);
+        sub(25,5,5);
+        sub(30,15,8);
+    }
