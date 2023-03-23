7673e2d2e2d51d36769942bfa253c90f4462c6d8
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2023-03-24 00:04:35 +0600

    1030A

---
+++ CodeForce/Problem_26.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int array[n];
+
+    for(int i=0; i<n; i++){
+        cin>>array[i];
+    }
+
+    for(int i=0; i<n; i++){
+        if(array[i] == 1){
+            cout<<"HARD"<<endl;
+            return 0;
+        }
+    }
+    cout<<"EASY"<<endl;
+}
