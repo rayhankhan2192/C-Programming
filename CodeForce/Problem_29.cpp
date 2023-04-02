7c9b0e7ee5f7b448a1028b50585e56b966def387
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2023-04-02 21:44:42 +0600

    A. Magnets (344A)

--- CodeForce/Problem_29.cpp
+++ CodeForce/Problem_29.cpp
@@ -18,3 +18,27 @@ int main()
 
     cout << counts << endl;
 }
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    int counts = 1;
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int current = arr[0];
+     for (int i = 0; i < n; i++){
+        if(arr[i] != current){
+            counts++;
+            current = arr[i];
+        }
+    }
+    cout << counts << endl;
+    
+}
+*/
