20672817163566c321ee36ab2706239a52f42432
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2023-04-05 16:18:13 +0600

    A. I Wanna Be the Guy (469A)

---
+++ CodeForce/Problem_35.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n+1] = {0};
+    int p,q,level;
+
+    cin>>p;
+    for(int i=0; i<p; i++){
+        cin>>level;
+        arr[level]=1;
+    }
+    cin>>q;
+    for(int i=0; i<q; i++){
+        cin>>level;
+        arr[level]=1;
+    }
+    for(int i=1; i<=n; i++){
+        if(arr[i] == 0){
+            cout<<"Oh, my keyboard!"<<endl;
+            return 0;
+        }
+    }
+    cout<<"I become the guy."<<endl;
+    return 0;
+}
